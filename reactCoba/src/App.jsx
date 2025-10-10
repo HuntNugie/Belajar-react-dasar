cae96d976965c0d41e6dd2b16fcb6a44ccd0cbfa
Nugie kurniawan <nugiebinhar@gmail.com>
2025-10-10 20:52:36 +0700

    done

--- reactCoba/src/App.jsx
+++ reactCoba/src/App.jsx
@@ -7,8 +7,6 @@ function App() {
     <>
      <div className="app">
       <Hello/>
-      <Hello/>
-      <Hello/>
      </div>
     </>
   )
--- reactCoba/src/components/helloWorld.jsx
+++ reactCoba/src/components/helloWorld.jsx
@@ -1,3 +1,13 @@
 export const Hello = ()=>{
-    return <h1>Hello dunia world Nugie kurniawan</h1>
+    const nama = "Nugie kurniawan"
+    const bahasa = ["Javascript","python","php"]
+    return <>
+        <h1>Hello dunia world {nama}</h1>
+        <h2>{bahasa.map(el=>{
+            return <>
+            <span>{el}</span> 
+            <br/>
+            </>
+        })}</h2>
+    </>
 }
