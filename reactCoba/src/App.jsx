db8c872c772b06dd27a44481cb2207c4068d7832
Nugie kurniawan <nugiebinhar@gmail.com>
2025-10-10 15:45:39 +0700

    belajar component react dasar

---
+++ reactCoba/src/App.jsx
@@ -0,0 +1,17 @@
+import { Hello } from './components/helloWorld'
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+     <div className="app">
+      <Hello/>
+      <Hello/>
+      <Hello/>
+     </div>
+    </>
+  )
+}
+
+export default App
---
+++ reactCoba/src/components/helloWorld.jsx
@@ -0,0 +1,3 @@
+export const Hello = ()=>{
+    return <h1>Hello dunia world Nugie kurniawan</h1>
+}
