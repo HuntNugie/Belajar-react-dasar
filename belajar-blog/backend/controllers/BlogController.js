01fd18df4d9c92ba2fafeb279540111414d3f23c
Nugie kurniawan <nugiebinhar@gmail.com>
2025-11-29 22:05:28 +0700

    belajarBlog:backend membuat route untuk filter

--- belajar-blog/backend/controllers/BlogController.js
+++ belajar-blog/backend/controllers/BlogController.js
@@ -14,3 +14,24 @@ export const index = async (req, res) => {
         return res.status(404).json(error);
     }
 };
+
+// untuk mengambil beberapa data blog berdasarkan query parameter
+export const show = async(req,res)=>{
+    try {
+        const limit = parseInt(req.query.limit);
+        if(limit){
+            const data = await Blog.findMany({
+                take:limit,
+                include:{
+                    author:true,
+                    categories:true
+                }
+            });
+            return res.status(200).json(data)
+        }else{
+            throw ({message:"format penggunaan salah"})
+        }
+    } catch (error) {
+        res.status(400).json(error)
+    }
+}
--- belajar-blog/backend/routes/blog.route.js
+++ belajar-blog/backend/routes/blog.route.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { index } from "../controllers/BlogController.js";
+import { index, show } from "../controllers/BlogController.js";
 
 const blogRoute = Router();
 
+// untuk filter mau berapa banyak
+blogRoute.get("/query",show)
+
+// untuk mengambil seluruh nya
 blogRoute.get("/all",index)
 
 export default blogRoute
