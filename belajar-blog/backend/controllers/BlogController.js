1eb3f5ee3558a1a24945d7e6cf424aaffdb71e34
Nugie kurniawan <nugiebinhar@gmail.com>
2025-11-29 20:57:32 +0700

    belajarBlog:backend migrate table created_at

--- belajar-blog/backend/controllers/BlogController.js
+++ belajar-blog/backend/controllers/BlogController.js
@@ -1,10 +1,16 @@
-import Blog from "../models/Blog.js"
-export const index = async(req,res)=>{
-    const data = await Blog.findMany({
-        include:{
-            author:true,
-            categories:true
-        }
-    })
-    return res.status(200).json(data);
-}
+import Blog from "../models/Blog.js";
+
+// untuk mengambil semua data blog
+export const index = async (req, res) => {
+    try {
+        const data = await Blog.findMany({
+            include: {
+                author: true,
+                categories: true,
+            },
+        });
+        return res.status(200).json(data);
+    } catch (error) {
+        return res.status(404).json(error);
+    }
+};
