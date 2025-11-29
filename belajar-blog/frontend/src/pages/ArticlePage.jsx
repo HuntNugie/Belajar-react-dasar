7c32727eaa63cdee9925e054bed59c651f56f83f
Nugie kurniawan <nugiebinhar@gmail.com>
2025-11-29 21:29:35 +0700

    belajarBlog:frontend membuat loading

--- belajar-blog/frontend/src/pages/ArticlePage.jsx
+++ belajar-blog/frontend/src/pages/ArticlePage.jsx
@@ -3,14 +3,17 @@ import ArticleCard from "../components/Article/ArticleCard";
 import Search from "../components/Article/Search";
 import {useState} from "react";
 import axios from "axios";
+import { LoadingArticleCard } from "../components/LoadingArticleCard";
 export default function ArticlePage() {
     const [post, setPost] = useState([]);
+    const [loading,setLoading] = useState(true);
     const api = import.meta.env.VITE_BACKEND_API;
     useEffect(() => {
         const request = async () => {
             const res = await axios.get(`${api}/blog/all`);
             const data = res.data;
             setPost(data);
+            setLoading(false)
         };
         request();
     }, []);
@@ -19,7 +22,7 @@ export default function ArticlePage() {
             <Search />
             {/* Articles Grid */}
             <div id="articlesGrid" className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                {post.map((el) => (
+                {loading ? (<LoadingArticleCard/>) : post.map((el) => (
                     <>
                         <ArticleCard data={el} />
                     </>
