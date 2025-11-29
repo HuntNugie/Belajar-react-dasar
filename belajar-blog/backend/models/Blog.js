4ea92fee881a80151d2731d1e34310839830cb98
Nugie kurniawan <nugiebinhar@gmail.com>
2025-11-29 08:36:36 +0700

    belajarBlog:backend models

---
+++ belajar-blog/backend/models/Blog.js
@@ -0,0 +1,5 @@
+import prisma from "./client.js";
+
+const Blog = prisma.blog;
+
+export default Blog;
---
+++ belajar-blog/backend/models/Category.js
@@ -0,0 +1,5 @@
+import prisma from "./client.js";
+
+const Category = prisma.category;
+
+export default Category;
