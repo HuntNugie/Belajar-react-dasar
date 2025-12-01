386320831156f672ec3da7e76789d68ceb3c22fd
Nugie kurniawan <nugiebinhar@gmail.com>
2025-12-01 15:24:34 +0700

    menambahkan data done

--- belajar-blog/frontend/src/components/AddBlog/Form.jsx
+++ belajar-blog/frontend/src/components/AddBlog/Form.jsx
@@ -8,9 +8,33 @@ export const Form = () => {
         category_id: "",
         body: "",
     });
-    const [category, setCategory] = useState([]);
 
     const api = import.meta.env.VITE_BACKEND_API;
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        // kirim ke backend
+        const res = await axios.post(`${api}/blog/add-blog`, form);
+        const data = res.data;
+        console.log(data);
+        
+        Swal.fire({
+            title: "berhasil menambahkan data blog",
+            icon: "success",
+            draggable: true,
+        });
+        e.target.reset()
+        
+    };
+
+    const handleChange = (e) => {
+        setForm((prev) => {
+            return {
+                ...prev,
+                [e.target.name]: e.target.value,
+            };
+        });
+    };
+    const [category, setCategory] = useState([]);
 
     useEffect(() => {
         // mengambil data category
@@ -23,12 +47,14 @@ export const Form = () => {
     }, []);
     return (
         <>
-            <form className="space-y-5">
+            <form className="space-y-5" onSubmit={handleSubmit}>
                 {/* Nama Author */}
                 <div>
                     <label className="block text-gray-300 mb-1">Nama Author</label>
                     <input
                         type="text"
+                        name="author"
+                        onChange={handleChange}
                         className="w-full px-4 py-2 rounded-lg bg-gray-700 text-white focus:ring-2 focus:ring-blue-500 outline-none"
                         placeholder="Misalnya: Nugie"
                     />
@@ -38,6 +64,8 @@ export const Form = () => {
                     <label className="block text-gray-300 mb-1">Judul Post</label>
                     <input
                         type="text"
+                        name="title"
+                        onChange={handleChange}
                         className="w-full px-4 py-2 rounded-lg bg-gray-700 text-white focus:ring-2 focus:ring-blue-500 outline-none"
                         placeholder="Masukkan judul posts"
                     />
@@ -45,10 +73,18 @@ export const Form = () => {
                 {/* Category Dropdown */}
                 <div>
                     <label className="block text-gray-300 mb-1">Kategori</label>
-                    <select className="w-full px-4 py-2 rounded-lg bg-gray-700 text-white appearance-none focus:ring-2 focus:ring-blue-500 outline-none">
+                    <select
+                        name="category_id"
+                        onChange={handleChange}
+                        className="w-full px-4 py-2 rounded-lg bg-gray-700 text-white appearance-none focus:ring-2 focus:ring-blue-500 outline-none"
+                    >
                         <option value="">Pilih kategori</option>
-                        {category.map((el)=>{
-                            return <option value={el.id} key={el.id}>{el.nama_category}</option>
+                        {category.map((el) => {
+                            return (
+                                <option value={el.id} key={el.id}>
+                                    {el.nama_category}
+                                </option>
+                            );
                         })}
                     </select>
                 </div>
@@ -57,6 +93,8 @@ export const Form = () => {
                     <label className="block text-gray-300 mb-1">Body Posts</label>
                     <textarea
                         rows={6}
+                        name="body"
+                        onChange={handleChange}
                         className="w-full px-4 py-2 rounded-lg bg-gray-700 text-white focus:ring-2 focus:ring-blue-500 outline-none resize-none"
                         placeholder="Tulis isi postingan di sini…"
                         defaultValue={""}
